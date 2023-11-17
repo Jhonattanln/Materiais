41ecfc2b19ee92312f2252fb6829ea6557480379
Valuup <jhonattanlino@outlook.com>
2023-11-17 10:21:06 -0300

    config: add files and code

---
+++ Code.R
@@ -0,0 +1,13 @@
+##### Instalando Bibliotecas #####
+install.packages("MASS") # Para datasets
+install.packages("ggplot2") # Para gráficos
+install.packages("dplyr") # Para manipulação de dados
+install.packages("ggResidpanel") # Para gráficos de resíduos
+install.packages("ISLR") # Para datasets
+
+##### Carregando Bibliotecas #####
+library(MASS)
+library(ggplot2)
+library(dplyr)
+library(ggResidpanel)
+library(ISLR)
